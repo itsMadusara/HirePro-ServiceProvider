1cb3406c0fa24487474c2dc96a0b0ddbf3e90c87
Tharushka Madusara <tharushka.madusara1234@gmail.com>
2023-07-31 23:08:13 +0530

    "SP Register API"

---
+++ Server/routes/user-register.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import pool from '../dbcon.js';
+import bcrypt from 'bcrypt';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+      const password = req.body.password.toString();
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const query = {
+        text: 'INSERT INTO public."ServiceProvider" (contact, name, email, nic, password_hash) VALUES ($1, $2, $3, $4, $5)',
+        values: [req.body.contact, req.body.name, req.body.email, req.body.nic, hashedPassword]
+      }
+      const newUser = await pool.query(query);
+      console.log(newUser);
+      res.json(newUser);
+    } catch (error) {
+      res.status(500).json({error: error.message});
+    }
+});
+
+export default router;
