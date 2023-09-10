232aca02e5ae52cf6242e5921992ab0b71c8e5f9
Sachini Muthugala <sachinimuthugala99@gmail.com>
2023-09-10 10:59:51 +0530

    modify categories

--- Client/HirePro/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Client/HirePro/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import geolocator_apple
 import firebase_core
 import firebase_storage
+import geolocator_apple
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
